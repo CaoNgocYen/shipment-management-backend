762f702a38196bb43648ba835f4643ab14e67e18
Cao Ngoc Yen <caongocyen@Caos-MacBook-Pro.local>
2022-04-23 14:50:39 +0200

    init

---
+++ src/main/java/de/htwberlin/webtech/ShippingTrackingApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingTrackingApplication.class, args);
+	}
+
+}
